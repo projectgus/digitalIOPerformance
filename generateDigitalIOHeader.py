051017b24a4bdff15a620e5ecdecf8f036e4a313
Angus Gratton <gus@projectgus.com>
2013-02-13 17:55:28 +1100

    Add feature to transparently improve digital I/O performance by default

--- generateDigitalIOHeader.py
+++ generateDigitalIOHeader.py
@@ -227,6 +227,21 @@ HEADER_SUFFIX = """
 #error "This header's Arduino configuration heuristics couldn't match this board configuration. No fast I/O is available. The header may be out of date."
 #endif
 #undef _DIGITALIO_MATCHED_BOARD
+
+#ifndef DIGITALIO_MANUAL
+#ifdef DIGITALIO_NO_INTERRUPTS
+#define digitalWrite digitalWriteFast
+#define digitalRead digitalReadFast
+#define pinMode pinModeFast
+#else
+#define digitalWrite digitalWriteSafe
+#define digitalRead digitalReadSafe
+#define pinMode pinModeSafe
+#endif
+
+#endif
+
+
 #endif
 """
 
